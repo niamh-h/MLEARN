c53dbf689765fedd7a89023d31f2d3d15cb0f3b6
niamh-h <74422377+niamh-h@users.noreply.github.com>
2021-07-30 16:15:26 +0100

    Update and rename validation.py to fn-finder/validation.py

--- fn-finder/validation.py
+++ fn-finder/validation.py
@@ -7,62 +7,62 @@ import sys
 #load the model
 clf = joblib.load('/mnt/c/Users/Niamh/Documents/SummerJob/data/16m_gdwbls/hf_uncal_clf/fnfinder_16wbls.sav')
 
-#load data
-data2 = pd.read_csv('/mnt/c/Users/Niamh/Documents/SummerJob/data/16m_gdwbls/hf_uncal_clf/hf_uncal_clf/li9_clfData16.txt', sep= " ", header=None)
+#load validation data
+data2 = pd.read_csv('/path/to/li9/txt/file', sep= " ", header=None)
 data2.columns = ['n9', 'n9_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
                 'beta_three_prev', 'beta_four', 'beta_four_prev', 'beta_five', 'beta_five_prev', 'beta_six', 'beta_six_prev', 'good_pos', 'good_pos_prev', 'closestPMT']
 df2 = pd.DataFrame(data2)
 df2['label'] = 0
 df2['source'] = 3 
 
-data3 = pd.read_csv('/mnt/c/Users/Niamh/Documents/SummerJob/data/16m_gdwbls/hf_uncal_clf/hf_uncal_clf/n17_clfData16.txt', sep= " ", header=None)
+data3 = pd.read_csv('/path/to/n17/txt/file', sep= " ", header=None)
 data3.columns = ['n9', 'n9_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
                 'beta_three_prev', 'beta_four', 'beta_four_prev', 'beta_five', 'beta_five_prev', 'beta_six', 'beta_six_prev', 'good_pos', 'good_pos_prev', 'closestPMT']
 df3 = pd.DataFrame(data3)
 df3['label'] = 0
 df3['source'] = 4 
 
-data5 = pd.read_csv('/mnt/c/Users/Niamh/Documents/SummerJob/data/16m_gdwbls/hf_uncal_clf/hf_uncal_clf/world_clfData16.txt', sep= " ", header=None)
+data5 = pd.read_csv('/path/to/world/txt/file', sep= " ", header=None)
 data5.columns = ['n9', 'n9_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
                 'beta_three_prev', 'beta_four', 'beta_four_prev', 'beta_five', 'beta_five_prev', 'beta_six', 'beta_six_prev', 'good_pos', 'good_pos_prev', 'closestPMT']
 df5 = pd.DataFrame(data5)
 df5['label'] = 0
 df5['source'] = 5
 
-datat = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/16m_gdwbls/hf_uncal_clf/hf_uncal_clf/tornessfull_clfData16.txt", sep=" ", header=None)
+datat = pd.read_csv("/path/to/tornessfull/txt/file", sep=" ", header=None)
 datat.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
                 "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT"]
 dft = pd.DataFrame(datat)
 dft['label'] = 0
 dft['source'] = 5
 
-datah = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/16m_gdwbls/hf_uncal_clf/hf_uncal_clf/heyshamfull_clfData16.txt", sep=" ", header=None)
+datah = pd.read_csv("/path/to/heyshamfull/txt/file", sep=" ", header=None)
 datah.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
                 "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT"]
 dfh = pd.DataFrame(datah)
 dfh['label'] = 0
 dfh['source'] = 1
 
-data6 = pd.read_csv('/mnt/c/Users/Niamh/Documents/SummerJob/data/16m_gdwbls/hf_uncal_clf/hf_uncal_clf/fn_clfData16.txt', sep= " ", header=None)
+data6 = pd.read_csv('/path/to/fn/txt/file', sep= " ", header=None)
 data6.columns = ['n9', 'n9_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
                 'beta_three_prev', 'beta_four', 'beta_four_prev', 'beta_five', 'beta_five_prev', 'beta_six', 'beta_six_prev', 'good_pos', 'good_pos_prev', 'closestPMT']
 df6 = pd.DataFrame(data6)
 df6['label'] = 1
 df6['source'] = 6
 
-data7 = pd.read_csv('/mnt/c/Users/Niamh/Documents/SummerJob/data/16m_gdwbls/hf_uncal_clf/hf_uncal_clf/geo_clfData16.txt', sep= " ", header=None)
+data7 = pd.read_csv('/path/to/geo/txt/file', sep= " ", header=None)
 data7.columns = ['n9', 'n9_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
                 'beta_three_prev', 'beta_four', 'beta_four_prev', 'beta_five', 'beta_five_prev', 'beta_six', 'beta_six_prev', 'good_pos', 'good_pos_prev', 'closestPMT']
 df7 = pd.DataFrame(data7)
 df7['label'] = 0
 df7['source'] = 7
 
-#data8 = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/16m_gdwbls/t+h_uncal_clf/t+h_uncal_clf/heysham2_clfData16.txt", sep=" ", header=None)
-#data8.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
-#                "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT"]
-#df8 = pd.DataFrame(data8)
-#df8['label'] = 0
-#df8['source'] = 1
+data8 = pd.read_csv("/path/to/heysham2/txt/file", sep=" ", header=None)
+data8.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
+                "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT"]
+df8 = pd.DataFrame(data8)
+df8['label'] = 0
+df8['source'] = 1
 
 frames = [df2, df3, df5, dft, df7, dfh]
 X = df6.append(frames, ignore_index=True)
@@ -73,22 +73,24 @@ source = X[['source']]
 source = source.to_numpy()
 source = source.flatten()
 X = X.drop(['label', 'source'], axis=1)
+#apply the model
 pred = clf.predict(X)
 prob = clf.predict_proba(X)
 scores = clf.decision_function(X)
+#analyse the model
 print(confusion_matrix(y, pred))
 print(classification_report(y, pred))
 disp = plot_confusion_matrix(clf, X, y)
-disp.figure_.suptitle('Fast Neutron Finder, Heysham full Signal (final validation, 16m_gdwbls)')
-plt.savefig('/mnt/c/Users/Niamh/Documents/SummerJob/figures/16m_gdwbls/hf_uncal/cm_clf16wbls.png')
+disp.figure_.suptitle('Confusion Matrix title')
+plt.savefig('/path/to/file')
 plt.show()
 
 plot_roc_curve(clf, X, y, pos_label=0, marker=',', label='Fast Neutron')
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.legend(loc='best')
-plt.title('Fast Neutron finder, Heysham full signal (final validation, 16m_gdwbls)')
-plt.savefig('/mnt/c/Users/Niamh/Documents/SummerJob/figures/16m_gdwbls/hf_uncal/roc_clf16wbls.png')
+plt.title('ROC Curve title')
+plt.savefig('/path/to/file')
 plt.show()
 
 X.loc[:,'classifier'] = pred
@@ -97,6 +99,6 @@ X.loc[:,'label'] = y
 X.loc[:,'scores'] = scores
 X.loc[:,'prob_fn'] = prob[:,1]
 X.loc[:,'prob_other'] = prob[:,0]
-print(X)
-X.to_csv('/mnt/c/Users/Niamh/Documents/SummerJob/data/16m_gdwbls/hf_uncal_clf/16wbls_clfclfdata.csv')
+
+X.to_csv('/path/to/file')
 
