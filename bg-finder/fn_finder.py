27008a15a56ce6b943bdd2355796d66110bc63ac
niamh-h <74422377+niamh-h@users.noreply.github.com>
2021-08-18 11:48:11 +0100

    Update fn_finder.py

--- bg-finder/fn_finder.py
+++ bg-finder/fn_finder.py
@@ -8,48 +8,48 @@ from matplotlib import pyplot as plt
 import joblib
 import sys
 #organise the data from the text files into a dataset
-data2 = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/22m_gdwbls/test/li9_testData.txt", sep=" ", header=None)
+data2 = pd.read_csv("/path/to/li9_data.txt", sep=" ", header=None)
 data2.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev", "beta_two", "beta_two_prev", "beta_three",
                 "beta_three_prev", "beta_four", "beta_four_prev", "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", "closestPMT_prev", "drPrevr"]
 df2 = pd.DataFrame(data2)
 df2['label'] = 0
 
-data3 = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/22m_gdwbls/test/n17_testData.txt", sep=" ", header=None)
+data3 = pd.read_csv("/path/to/n17_data.txt", sep=" ", header=None)
 data3.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev",  "beta_three",
                 "beta_three_prev","beta_four", "beta_four_prev", "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", "closestPMT_prev", "drPrevr"]
 df3 = pd.DataFrame(data3)
 df3['label'] = 0
 
-data5 = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/22m_gdwbls/test/world_testData.txt", sep=" ", header=None)
+data5 = pd.read_csv("/path/to/world_data.txt", sep=" ", header=None)
 data5.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
                 "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", "closestPMT_prev", "drPrevr"]
 df5 = pd.DataFrame(data5)
 df5['label'] = 0
-datat = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/22m_gdwbls/test/tornessfull_testData.txt", sep=" ", header=None)
+datat = pd.read_csv("path/to/torness_data.txt", sep=" ", header=None)
 datat.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
                 "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", "closestPMT_prev", "drPrevr"]
 dft = pd.DataFrame(datat)
 dft['label'] = 0
 
-#datah = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/16m_gdwbls/test/heyshamfull_testData.txt", sep=" ", header=None)
+#datah = pd.read_csv("/path/to/heyshamfull_data.txt", sep=" ", header=None)
 #datah.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
 #                "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", "closestPMT_prev", "drPrevr"]
 #dfh = pd.DataFrame(datah)
 #dfh['label'] = 0
 
-data6 = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/22m_gdwbls/test/fn_testData.txt", sep=" ", header=None)
+data6 = pd.read_csv("/path/to/neutrons_data.txt", sep=" ", header=None)
 data6.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
                 "beta_three_prev", "beta_four", "beta_four_prev", "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", "closestPMT_prev", "drPrevr"]
 df6 = pd.DataFrame(data6)
 df6['label'] = 1
 
-data7 = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/22m_gdwbls/test/geo_testData.txt", sep=" ", header=None)
+data7 = pd.read_csv("/path/to/geoneutrinos_data.txt", sep=" ", header=None)
 data7.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
                 "beta_three_prev", "beta_four", "beta_four_prev", "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", "closestPMT_prev", "drPrevr"]
 df7 = pd.DataFrame(data7)
 df7['label'] = 0
 
-data8 = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/22m_gdwbls/test/heysham2_testData.txt", sep=" ", header=None)
+data8 = pd.read_csv("/path/to/heysham2_data.txt", sep=" ", header=None)
 data8.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
                 "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", "closestPMT_prev", "drPrevr"]
 df8 = pd.DataFrame(data8)
@@ -72,7 +72,7 @@ neu_score = neu_clf.decision_function(test_neu)
 print(confusion_matrix(test_neulab, neu_predictions))
 disp = plot_confusion_matrix(neu_clf, test_neu, test_neulab)
 disp.figure_.suptitle("Fast Neutron finder, Heysham 2 signal (training, 22m_gdwbls)")
-plt.savefig('/mnt/c/Users/Niamh/Documents/SummerJob/figures/22m_gdwbls/h2_uncal/cm_test22wbls.png')
+plt.savefig('/path/to/file.png')
 plt.show()
 print(classification_report(test_neulab, neu_predictions))
 test_neu.loc[:,'classifier'] = neu_predictions
@@ -90,9 +90,9 @@ plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.legend(loc='best')
 plt.title('Fast Neutron finder, Heysham 2 Signal (training, 22m_gdwbls)')
-plt.savefig('/mnt/c/Users/Niamh/Documents/SummerJob/figures/22m_gdwbls/h2_uncal/roc_test22wbls.png')
+plt.savefig('/path/to/file.png')
 plt.show()
 
-test_neu.to_csv('/mnt/c/Users/Niamh/Documents/SummerJob/data/22m_gdwbls/h2_uncal_clf/fn_finder/22wbls_clftestdata.csv')
-filename = '/mnt/c/Users/Niamh/Documents/SummerJob/data/22m_gdwbls/h2_uncal_clf/fn_finder/fnfinder_22wbls.sav'
+test_neu.to_csv('/path/to/file.csv')
+filename = '/path/to/file.sav'
 joblib.dump(neu_clf, filename)
