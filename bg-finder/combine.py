d1cb83e1398f2d088156e69d516f7742aa87e435
niamh-h <74422377+niamh-h@users.noreply.github.com>
2021-08-23 16:44:28 +0100

    Update combine.py

--- bg-finder/combine.py
+++ bg-finder/combine.py
@@ -5,59 +5,52 @@ from matplotlib import pyplot as plt
 from sklearn.metrics import plot_roc_curve, confusion_matrix, plot_confusion_matrix, classification_report, roc_auc_score, auc
 import sys
 #load the models
-li9_clf = joblib.load('/mnt/c/Users/Niamh/Documents/SummerJob/data_v2/16m_wbls/li9finder_16wbls.sav')
-fn_clf = joblib.load('/mnt/c/Users/Niamh/Documents/SummerJob/data_v2/16m_wbls/fnfinder_16wbls.sav')
+li9_clf = joblib.load('/path/to/lithium/model.sav')
+fn_clf = joblib.load('/path/to/neutron/model.sav')
 #load data
-data2 = pd.read_csv('/mnt/c/Users/Niamh/Documents/SummerJob/data_v2/16m_wbls/clf_2/li9_clf.txt', sep= " ", header=None)
+data2 = pd.read_csv('/path/to/lithium/file.txt', sep= " ", header=None)
 data2.columns = ['n9', 'n9_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
                 'beta_three_prev', 'beta_four', 'beta_four_prev', 'beta_five', 'beta_five_prev', 'beta_six', 'beta_six_prev', 'good_pos', 'good_pos_prev', 'closestPMT', 'closestPMT_prev', 'drPrevr', 'mc_energy']
 df2 = pd.DataFrame(data2)
 df2['label'] = 0
 df2['source'] = 2
 
-data3 = pd.read_csv('/mnt/c/Users/Niamh/Documents/SummerJob/data_v2/16m_wbls/clf_2/n17_clf.txt', sep= " ", header=None)
+data3 = pd.read_csv('/path/to/nitrogen/file.txt', sep= " ", header=None)
 data3.columns = ['n9', 'n9_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
                 'beta_three_prev', 'beta_four', 'beta_four_prev', 'beta_five', 'beta_five_prev', 'beta_six', 'beta_six_prev', 'good_pos', 'good_pos_prev', 'closestPMT', 'closestPMT_prev', 'drPrevr', 'mc_energy']
 df3 = pd.DataFrame(data3)
 df3['label'] = 0
 df3['source'] = 3
 
-data5 = pd.read_csv('/mnt/c/Users/Niamh/Documents/SummerJob/data_v2/16m_wbls/clf_2/world_clf.txt', sep= " ", header=None)
+data5 = pd.read_csv('path/to/world/file.txt', sep= " ", header=None)
 data5.columns = ['n9', 'n9_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
                 'beta_three_prev', 'beta_four', 'beta_four_prev', 'beta_five', 'beta_five_prev', 'beta_six', 'beta_six_prev', 'good_pos', 'good_pos_prev', 'closestPMT', 'closestPMT_prev', 'drPrevr', 'mc_energy']
 df5 = pd.DataFrame(data5)
 df5['label'] = 0
 df5['source'] = 4
 
-datat = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data_v2/16m_wbls/clf_2/tornessfull_clf.txt", sep=" ", header=None)
+datat = pd.read_csv("/path/to/torness/file.txt", sep=" ", header=None)
 datat.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
                 "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", 'closestPMT_prev', 'drPrevr', 'mc_energy']
 dft = pd.DataFrame(datat)
 dft['label'] = 0
 dft['source'] = 5
 
-#datah = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/16m_gdwbls/hf_uncal_clf/hf_uncal_clf/heyshamfull_clfData16.txt", sep=" ", header=None)
-#datah.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
-#                "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_pr>
-#dfh = pd.DataFrame(datah)
-#dfh['label'] = 0
-#dfh['source'] = 1
-
-data6 = pd.read_csv('/mnt/c/Users/Niamh/Documents/SummerJob/data_v2/16m_wbls/clf_2/fn_clf.txt', sep= " ", header=None)
+data6 = pd.read_csv('/path/to/neutron/file.txt', sep= " ", header=None)
 data6.columns = ['n9', 'n9_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
                 'beta_three_prev', 'beta_four', 'beta_four_prev', 'beta_five', 'beta_five_prev', 'beta_six', 'beta_six_prev', 'good_pos', 'good_pos_prev', 'closestPMT', 'closestPMT_prev', 'drPrevr', 'mc_energy']
 df6 = pd.DataFrame(data6)
 df6['label'] = 1
 df6['source'] = 6
 
-data7 = pd.read_csv('/mnt/c/Users/Niamh/Documents/SummerJob/data_v2/16m_wbls/clf_2/geo_clf.txt', sep= " ", header=None)
+data7 = pd.read_csv('/path/to/geoneutrino/file.txt', sep= " ", header=None)
 data7.columns = ['n9', 'n9_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
                 'beta_three_prev', 'beta_four', 'beta_four_prev', 'beta_five', 'beta_five_prev', 'beta_six', 'beta_six_prev', 'good_pos', 'good_pos_prev', 'closestPMT', 'closestPMT_prev', 'drPrevr', 'mc_energy']
 df7 = pd.DataFrame(data7)
 df7['label'] = 0
 df7['source'] = 7
 
-data8 = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data_v2/16m_wbls/clf_2/heysham2_clf.txt", sep=" ", header=None)
+data8 = pd.read_csv("/path/to/heysham/file.txt", sep=" ", header=None)
 data8.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
                 "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", 'closestPMT_prev', 'drPrevr', 'mc_energy']
 df8 = pd.DataFrame(data8)
@@ -83,16 +76,16 @@ print(confusion_matrix(fn_lab, fn_pred))
 print(classification_report(fn_lab, fn_pred))
 disp = plot_confusion_matrix(fn_clf, fn, fn_lab)
 disp.figure_.suptitle('Combined: Fast Neutron Finder, Heysham 2 Signal (16m, gdwbls)')
-#plt.savefig('/mnt/c/Users/Niamh/Documents/SummerJob/figures_v2/16m_wbls/fncm_clf16wbls_h2.png')
-#plt.show()
+plt.savefig('/path/to/file.png')
+plt.show()
 
 plot_roc_curve(fn_clf, fn, fn_lab, pos_label=0, marker=',', label='Fast Neutron')
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.legend(loc='best')
 plt.title('Combined: Fast Neutron finder, Heysham 2 signal (16m, gdwbls)')
-#plt.savefig('/mnt/c/Users/Niamh/Documents/SummerJob/figures_v2/16m_wbls/fnroc_clf16wbls_h2.png')
-#plt.show()
+plt.savefig('/path/to/file.png')
+plt.show()
 
 fn.loc[:,'fn_classifier'] = fn_pred
 fn.loc[:,'source'] = fn_source
@@ -102,13 +95,13 @@ fn.loc[:,'prob_fn'] = fn_prob[:,1]
 fn.loc[:,'prob_otherfn'] = fn_prob[:,0]
 fn.loc[:,'mc_energy'] = fn_en
 print(fn)
-fn.to_csv('/mnt/c/Users/Niamh/Documents/SummerJob/data_v2/16m_wbls/clf_2/fn_clf16wbls_h2.csv')
+fn.to_csv('/path/to/file.csv')
 
 li9 = fn.loc[fn.fn_classifier==0] #keep only what fn_finder leaves
 li9.label.replace(to_replace=1,value=0,inplace=True) #replace labels as 0
-li9.loc[li9.source==3, 'label'] = 1 #relabel li9 as 1
+li9.loc[li9.source==2, 'label'] = 1 #relabel li9 as 1
 print(li9)
-print(li9.loc[li9.source==3])
+print(li9.loc[li9.source==2])
 
 li9_lab = li9['label']
 li9_lab = li9_lab.to_numpy()
@@ -129,16 +122,16 @@ print(confusion_matrix(li9_lab, li9_pred))
 print(classification_report(li9_lab, li9_pred))
 disp = plot_confusion_matrix(li9_clf, li9, li9_lab)
 disp.figure_.suptitle('Combined: Lithium-9 Finder, Heysham 2 Signal (16m, gdwbls)')
-#plt.savefig('/mnt/c/Users/Niamh/Documents/SummerJob/figures_v2/16m_wbls/li9cm_clf16wbls_h2.png')
-#plt.show()
+plt.savefig('/path/to/file.png')
+plt.show()
 
 plot_roc_curve(li9_clf, li9, li9_lab, pos_label=0, marker=',', label='Lithium-9')
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.legend(loc='best')
 plt.title('Combined: Lithium-9 finder, Heysham 2 signal (16m, gdwbls)')
-#plt.savefig('/mnt/c/Users/Niamh/Documents/SummerJob/figures_v2/16m_wbls/li9roc_clf16wbls_h2.png')
-#plt.show()
+plt.savefig('/path/to/file.png')
+plt.show()
 
 li9.loc[:,'li9_classifier'] = li9_pred
 li9.loc[:,'source'] = li9_source
@@ -148,5 +141,5 @@ li9.loc[:,'prob_li9'] = li9_prob[:,1]
 li9.loc[:,'prob_otherli9'] = li9_prob[:,0]
 li9.loc[:,'mc_energy'] = li9_en
 print(li9)
-li9.to_csv('/mnt/c/Users/Niamh/Documents/SummerJob/data_v2/16m_wbls/clf_2/li9_clf16wbls_h2.csv')
+li9.to_csv('/path/to/file.csv')
 
