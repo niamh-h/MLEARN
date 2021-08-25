b464d8ac7fe2a2f7b11844c1c4baf96e885ec248
niamh-h <74422377+niamh-h@users.noreply.github.com>
2021-08-25 11:23:16 +0100

    Update gridsearch.py

--- bg-finder/gridsearch.py
+++ bg-finder/gridsearch.py
@@ -8,44 +8,44 @@ from matplotlib import pyplot as plt
 import joblib
 import sys
 #organise the data from the text files into a dataset
-data2 = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/22m_gdh20/test/li9_RawData22.txt", sep=" ", header=None)
+data2 = pd.read_csv("/path/to/lithium/file.txt", sep=" ", header=None)
 data2.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev", "beta_two", "beta_two_prev", "beta_three",
-                "beta_three_prev", "beta_four", "beta_four_prev", "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT"]
+                "beta_three_prev", "beta_four", "beta_four_prev", "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", "closestPMT_prev", "drPrevr"]
 df2 = pd.DataFrame(data2)
 df2['label'] = 0
 
-data3 = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/22m_gdh20/test/n17_RawData22.txt", sep=" ", header=None)
+data3 = pd.read_csv("/path/to/nitrogen/file.txt", sep=" ", header=None)
 data3.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev",  "beta_three",
-                "beta_three_prev","beta_four", "beta_four_prev", "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT"]
+                "beta_three_prev","beta_four", "beta_four_prev", "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", "closestPMT_prev", "drPrevr"]
 df3 = pd.DataFrame(data3)
 df3['label'] = 0
 
-data5 = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/22m_gdh20/test/world_RawData22.txt", sep=" ", header=None)
+data5 = pd.read_csv("/path/to/world/file.txt", sep=" ", header=None)
 data5.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
-                "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT"]
+                "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", "closestPMT_prev", "drPrevr"]
 df5 = pd.DataFrame(data5)
 df5['label'] = 0
-datat = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/22m_gdh20/test/tornessfull_RawData22.txt", sep=" ", header=None)
+datat = pd.read_csv("/path/to/torness/file.txt", sep=" ", header=None)
 datat.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
-                "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT"]
+                "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", "closestPMT_prev", "drPrevr"]
 dft = pd.DataFrame(datat)
 dft['label'] = 0
 
-data6 = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/22m_gdh20/test/fn_RawData22.txt", sep=" ", header=None)
+data6 = pd.read_csv("/path/to/neutrons/file.txt", sep=" ", header=None)
 data6.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
-                "beta_three_prev", "beta_four", "beta_four_prev", "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT"]
+                "beta_three_prev", "beta_four", "beta_four_prev", "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", "closestPMT_prev", "drPrevr"]
 df6 = pd.DataFrame(data6)
 df6['label'] = 1
 
-data7 = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/22m_gdh20/test/geo_RawData22.txt", sep=" ", header=None)
+data7 = pd.read_csv("/path/to/geo/file.txt", sep=" ", header=None)
 data7.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
-                "beta_three_prev", "beta_four", "beta_four_prev", "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT"]
+                "beta_three_prev", "beta_four", "beta_four_prev", "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", "closestPMT_prev", "drPrevr"]
 df7 = pd.DataFrame(data7)
 df7['label'] = 0
 
-data8 = pd.read_csv("/mnt/c/Users/Niamh/Documents/SummerJob/data/22m_gdh20/test/heysham_2_RawData22.txt", sep=" ", header=None)
+data8 = pd.read_csv("/path/to/heysham/file.txt", sep=" ", header=None)
 data8.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
-                "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT"]
+                "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", "closestPMT_prev", "drPrevr"]
 df8 = pd.DataFrame(data8)
 df8['label'] = 0
 
@@ -57,7 +57,7 @@ df6 = df6.head(200)
 df7 =df7.head(200)
 df8 = df8.head(200)
 
-#neutron model
+#neutron model example
 frames = [df3, df2, df5, dft, df7, df8]
 neu = df6.append(frames, ignore_index=True)
 neu_labelcolumn = neu[['label']]
@@ -65,7 +65,7 @@ neu_label = neu_labelcolumn.to_numpy()
 neu_label = neu_label.flatten()
 neu = neu.drop(['label'], axis=1)
 train_neu, test_neu, train_neulab, test_neulab = train_test_split(neu, neu_label, random_state=None, stratify=neu_label)
-
+#create empty model
 dtc = DecisionTreeClassifier()
 ada_classifier = AdaBoostClassifier(base_estimator=dtc)
 ada_classifier.fit(train_neu, train_neulab)
@@ -73,7 +73,7 @@ ada_predictions = ada_classifier.predict(test_neu)
 ada_pred_prob = ada_classifier.predict_proba(test_neu)
 
 #Grid search code
-grid = {"n_estimators" : [10, 50, 100, 200, 500],
+grid = {"n_estimators" : [10, 50, 100, 200, 500], #popular values of each argument
         "learning_rate" : [0.0001, 0.001, 0.01, 0.1, 1.0],
         "base_estimator__max_depth" : [1, 2, 3, 4, 5, 6, 7, 8, 10, 20, 30, 40]
         }
