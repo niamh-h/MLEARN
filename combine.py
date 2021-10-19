0b29cb0a42f7c47bdcd30ba3648d8acae9ce37f1
niamh-h <74422377+niamh-h@users.noreply.github.com>
2021-10-19 15:11:34 +0100

    Update combine.py

--- combine.py
+++ combine.py
@@ -2,62 +2,62 @@ import numpy as np
 import pandas as pd
 import joblib
 from matplotlib import pyplot as plt
-from sklearn.metrics import plot_roc_curve, confusion_matrix, plot_confusion_matrix, classification_report, roc_auc_score, auc
+from sklearn.metrics import roc_curve, confusion_matrix, ConfusionMatrixDisplay, classification_report, roc_auc_score, auc
 import sys
 #load the models
 li9_clf = joblib.load('/path/to/lithium/model.sav')
 fn_clf = joblib.load('/path/to/neutron/model.sav')
 #load data
+data1 = pd.read_csv("/path/to/heysham/file.txt", sep=" ", header=None)
+data1.columns = ["n100", "n100_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
+                "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", 'closestPMT_prev', 'drPrevr', 'mc_energy']
+df1 = pd.DataFrame(data1)
+df1['label'] = 0
+df1['source'] = 1
+
 data2 = pd.read_csv('/path/to/lithium/file.txt', sep= " ", header=None)
-data2.columns = ['n9', 'n9_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
+data2.columns = ['n100', 'n100_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
                 'beta_three_prev', 'beta_four', 'beta_four_prev', 'beta_five', 'beta_five_prev', 'beta_six', 'beta_six_prev', 'good_pos', 'good_pos_prev', 'closestPMT', 'closestPMT_prev', 'drPrevr', 'mc_energy']
 df2 = pd.DataFrame(data2)
 df2['label'] = 0
 df2['source'] = 2
 
 data3 = pd.read_csv('/path/to/nitrogen/file.txt', sep= " ", header=None)
-data3.columns = ['n9', 'n9_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
+data3.columns = ['n100', 'n100_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
                 'beta_three_prev', 'beta_four', 'beta_four_prev', 'beta_five', 'beta_five_prev', 'beta_six', 'beta_six_prev', 'good_pos', 'good_pos_prev', 'closestPMT', 'closestPMT_prev', 'drPrevr', 'mc_energy']
 df3 = pd.DataFrame(data3)
 df3['label'] = 0
 df3['source'] = 3
 
-data5 = pd.read_csv('path/to/world/file.txt', sep= " ", header=None)
-data5.columns = ['n9', 'n9_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
+data4 = pd.read_csv('path/to/world/file.txt', sep= " ", header=None)
+data4.columns = ['n100', 'n100_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
                 'beta_three_prev', 'beta_four', 'beta_four_prev', 'beta_five', 'beta_five_prev', 'beta_six', 'beta_six_prev', 'good_pos', 'good_pos_prev', 'closestPMT', 'closestPMT_prev', 'drPrevr', 'mc_energy']
+df4 = pd.DataFrame(data4)
+df4['label'] = 0
+df4['source'] = 4
+
+data5 = pd.read_csv("/path/to/torness/file.txt", sep=" ", header=None)
+data5.columns = ["n100", "n100_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
+                "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", 'closestPMT_prev', 'drPrevr', 'mc_energy']
 df5 = pd.DataFrame(data5)
 df5['label'] = 0
-df5['source'] = 4
-
-datat = pd.read_csv("/path/to/torness/file.txt", sep=" ", header=None)
-datat.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
-                "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", 'closestPMT_prev', 'drPrevr', 'mc_energy']
-dft = pd.DataFrame(datat)
-dft['label'] = 0
-dft['source'] = 5
+df5['source'] = 5
 
 data6 = pd.read_csv('/path/to/neutron/file.txt', sep= " ", header=None)
-data6.columns = ['n9', 'n9_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
+data6.columns = ['n100', 'n100_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
                 'beta_three_prev', 'beta_four', 'beta_four_prev', 'beta_five', 'beta_five_prev', 'beta_six', 'beta_six_prev', 'good_pos', 'good_pos_prev', 'closestPMT', 'closestPMT_prev', 'drPrevr', 'mc_energy']
 df6 = pd.DataFrame(data6)
 df6['label'] = 1
 df6['source'] = 6
 
 data7 = pd.read_csv('/path/to/geoneutrino/file.txt', sep= " ", header=None)
-data7.columns = ['n9', 'n9_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
+data7.columns = ['n100', 'n10_prev', 'dt_prev_us', 'inner_hit', 'inner_hit_prev', 'beta_one', 'beta_one_prev', 'beta_two', 'beta_two_prev', 'beta_three',
                 'beta_three_prev', 'beta_four', 'beta_four_prev', 'beta_five', 'beta_five_prev', 'beta_six', 'beta_six_prev', 'good_pos', 'good_pos_prev', 'closestPMT', 'closestPMT_prev', 'drPrevr', 'mc_energy']
 df7 = pd.DataFrame(data7)
 df7['label'] = 0
 df7['source'] = 7
 
-data8 = pd.read_csv("/path/to/heysham/file.txt", sep=" ", header=None)
-data8.columns = ["n9", "n9_prev", "dt_prev_us", "inner_hit", "inner_hit_prev", "beta_one", "beta_one_prev","beta_two", "beta_two_prev", "beta_three",
-                "beta_three_prev", "beta_four", "beta_four_prev",  "beta_five", "beta_five_prev", "beta_six", "beta_six_prev", "good_pos", "good_pos_prev", "closestPMT", 'closestPMT_prev', 'drPrevr', 'mc_energy']
-df8 = pd.DataFrame(data8)
-df8['label'] = 0
-df8['source'] = 1
-
-fn_frames = [df2, df3, df5, dft, df7, df8]
+fn_frames = [df1, df2, df3, df4, df5, df7]
 fn = df6.append(fn_frames, ignore_index=True)
 fn_lab = fn[['label']]
 fn_lab = fn_lab.to_numpy()
@@ -72,12 +72,12 @@ fn = fn.drop(['label', 'source', 'mc_energy'], axis=1)
 fn_pred = fn_clf.predict(fn)
 fn_prob = fn_clf.predict_proba(fn)
 fn_scores = fn_clf.decision_function(fn)
-print(confusion_matrix(fn_lab, fn_pred))
-print(classification_report(fn_lab, fn_pred))
-disp = plot_confusion_matrix(fn_clf, fn, fn_lab)
-disp.figure_.suptitle('Combined: Fast Neutron Finder, Heysham 2 Signal (16m, gdwbls)')
+cm = confusion_matrix(test_y,pred,labels=clf.classes_)
+print (cm)
+ConfusionMatrixDisplay.from_predictions(fn_lab, fn_pred)
+plt.title('Fast Neutron Finder confusion matrix')
 plt.savefig('/path/to/file.png')
-plt.show()
+#plt.show()
 
 plot_roc_curve(fn_clf, fn, fn_lab, pos_label=0, marker=',', label='Fast Neutron')
 plt.xlabel('False Positive Rate')
